cd021b35ad570aca43ddc2cedb75c1af73aab35c
jaimeMA <jma85@alu.ua.es>
2019-03-24 01:55:09 +0100

    graficos Solucinado
    listo graficos

--- Proyect/graficos.py
+++ Proyect/graficos.py
@@ -70,30 +70,3 @@ else:
     print ("Necesario ejecutar con al menos 4 parametros: ")
     print ("python3 nombrePrograma nombreAcciones nombreClicks nombrePulsTeclado")
 
-"""
-# Generamos los datos para el gráfico
-x = np.array(range(500))*0.1
-y = np.zeros(len(x))
-for i in range(len(x)):
-    y[i] = math.sin(x[i])
-
-# Generamos un segundo conjunto de datos para el gráfico
-x_2 = np.array(range(500))*0.1
-y_2 = np.zeros(len(x_2))
-for j in range(len(x_2)):
-    y_2[j] = math.cos(x_2[j])
-
-# Creamos el gráfico
-
-plt.plot(x,y,'g',x_2,y_2,'r, *')
-plt.show()
-
-#Colocamos las etiquetas de los ejes
-plt.xlabel("Coordenada X")
-plt.ylabel("Coordenada Y")
-
-#Colocamos la leyenda
-plt.legend(['Seno','Coseno'])
-
-#Colocamos el título del gráfico
-plt.title("Representacion de funciones")"""
